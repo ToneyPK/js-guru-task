feb17f44520078c1cd9379778cfdea677f2886e1
toneypk <antoniopekeljevic@gmail.com>
2020-10-31 19:46:07 +0100

    style CTA section

--- src/components/cta/CTA.js
+++ src/components/cta/CTA.js
@@ -4,15 +4,17 @@ import ctaImage from '../../assets/media/image-post.png'
 export default function CTA() {
   return (
     <div className="cta">
-      <img src={ctaImage} />
-      <div className="cta-content">
-        <h2>Post an Item for Sale</h2>
-        <p>
-          To post an item for sale - please register or sign in if you are
-          already registered with allgood.
-        </p>
-        <button className="red">Sign up</button>
-        <button>Sign In</button>
+      <div className="container ai-center d-flex">
+        <img src={ctaImage} />
+        <div className="cta-content">
+          <h2>Post an Item for Sale</h2>
+          <p>
+            To post an item for sale - please register or sign in if you are
+            already registered with allgood.
+          </p>
+          <button className="red">Sign up</button>
+          <button className="hollow">Sign In</button>
+        </div>
       </div>
     </div>
   )
