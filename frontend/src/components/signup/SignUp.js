2b80c70010729e21ee5a95e76265d1381899b36a
toneypk <antoniopekeljevic@gmail.com>
2020-11-05 18:23:01 +0100

    fix className bug in SignUp section

--- frontend/src/components/signup/SignUp.js
+++ frontend/src/components/signup/SignUp.js
@@ -2,22 +2,22 @@ import React from 'react'
 
 export default function SignUp() {
   return (
-    <div className="sign-up">
-      <div className="box">
-        <div className="content">
+    <div className='sign-up'>
+      <div className='box'>
+        <div className='content'>
           <h3>Are you a Charity or Organization looking to Fundraise?</h3>
           <p>
             Want to register with AllGood.market so people can donate the
             proceeds of their sales to you? Please contact us at{' '}
-            <a class="text-light" href="mailto:info@allgood.market">
+            <a className='text-light' href='mailto:info@allgood.market'>
               info@allgood.market
             </a>{' '}
             or complete out online form.
           </p>
-          <button className="red hvr-grow">Sign /up charity/campaign</button>
+          <button className='red hvr-grow'>Sign /up charity/campaign</button>
         </div>
-        <div className="outer-circle">
-          <div className="inner-circle"></div>
+        <div className='outer-circle'>
+          <div className='inner-circle'></div>
         </div>
       </div>
     </div>
